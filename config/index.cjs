8e9f4de38dd2e71e415f7c84f9ffa7901ab99ec8
zyjJava <106665943+zyjJava@users.noreply.github.com>
2022-10-08 16:07:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oO3Yj6cPVMqPA7D84Tz-cQUXyEN8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'VWJw97dmDVho1AwBQxomoMaroyrRX-2k7Tw4WWZRHk8',
+      useTemplateId: 'F0Ojeoo0dXW12voU6JLQejPnbs6WylDFQ_5aqRLvOAI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'VWJw97dmDVho1AwBQxomoMaroyrRX-2k7Tw4WWZRHk8',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oO3Yj6cPVMqPA7D84Tz-cQUXyEN8',
+      id: '',
     }
   ],
 
