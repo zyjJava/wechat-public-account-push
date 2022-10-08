be28c9ff7045015979106935dd01030de7a528fb
zyjJava <106665943+zyjJava@users.noreply.github.com>
2022-10-08 16:40:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '哈颖颖',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ['oO3Yj6cPVMqPA7D84Tz-cQUXyEN8','oO3Yj6d51sRA3EZuIohl2ILXR8nA'],
+      id: 'oO3Yj6cPVMqPA7D84Tz-cQUXyEN8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'KghBqcx3kLHqkCpqXN8mP6j91jVARrrG31W77z7qL_o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oO3Yj6cPVMqPA7D84Tz-cQUXyEN8',
     }
   ],
 
